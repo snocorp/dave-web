4cdd4f37d699b01bf21c96a4eca955138e0a71f0
Dave Sewell <snocorp@gmail.com>
2025-04-13 10:03:50 -0400

    Use sanity for pages (#3)
    * Use sanity for pages
    
    * Remove resume link

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,13 +1,5 @@
-const h = require("hyperscript");
-const blocksToHtml = require("@sanity/block-content-to-html");
-const sanity = require("@sanity/client");
-
-const client = sanity.createClient({
-  projectId: "gjs6ssl8",
-  dataset: "production",
-  useCdn: false, // set to `false` to bypass the edge cache
-  apiVersion: "2025-02-06",
-});
+const { client } = require("./utils/sanity");
+const { formatBody } = require("./utils/format");
 
 module.exports = async function () {
   const posts = await client.fetch(`*[_type == "post"]{
@@ -24,32 +16,6 @@ module.exports = async function () {
   //posts.forEach((p) => p.body.forEach(console.log));
   return posts.map((post) => ({
     ...post,
-    body: blocksToHtml({
-      blocks: post.body,
-      serializers: {
-        types: {
-          block: (props) => {
-            const style = props.node.style || "normal";
-
-            if (/^h\d/.test(style)) {
-              const level = style.replace(/[^\d]/g, "");
-              return h(
-                style,
-                { className: `is-size-${level}` },
-                props.children
-              );
-            }
-
-            return h("p", { className: "block" }, props.children);
-          },
-          code: (props) =>
-            h(
-              "pre",
-              { className: `${props.node.language} block` },
-              h("code", props.node.code)
-            ),
-        },
-      },
-    }),
+    body: formatBody(post.body),
   }));
 };
